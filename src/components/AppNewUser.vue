bf165f6b927e0979b7e7fb36594254db2869d2c1
xrr2016 <xiaoranran1993@163.com>
2018-09-06 04:04:52 +0800

    add bulma and AppNewUser component

---
+++ src/components/AppNewUser.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="new-user">
+    <h2 class="new-user__title">你的名字是？</h2>
+    <form @submit.prevent="createNewUser">
+      <input class="new-user__input" type="text" v-model="username" />
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppNewUser',
+  data() {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    createNewUser() {}
+  }
+}
+</script>
+
+<style scoped>
+.new-user {
+
+}
+</style>
